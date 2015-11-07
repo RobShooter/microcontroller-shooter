69bf598b65acebd885ed92ea3733a324229e893f
Kani <Kani@mail.de>
2015-11-07 17:04:24 +0100

    1

--- src/main/java/de/thi/microcontroller/web/domain/Microcontroller.java
+++ src/main/java/de/thi/microcontroller/web/domain/Microcontroller.java
@@ -1,5 +1,50 @@
 package de.thi.microcontroller.web.domain;
 
 
-public class Microcontroller {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Microcontroller implements Serializable{
+
+    private String name;
+    private String architektur;
+    private String bauform;
+    private List<String> schnittstellen = new ArrayList<>();
+
+    public Microcontroller(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArchitektur() {
+        return architektur;
+    }
+
+    public void setArchitektur(String architektur) {
+        this.architektur = architektur;
+    }
+
+    public String getBauform() {
+        return bauform;
+    }
+
+    public void setBauform(String bauform) {
+        this.bauform = bauform;
+    }
+
+    public List<String> getSchnittstellen() {
+        return schnittstellen;
+    }
+
+    public void setSchnittstellen(List<String> schnittstellen) {
+        this.schnittstellen = schnittstellen;
+    }
 }
